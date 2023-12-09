3bba0e48778f26cace95b649492bc9ffe22d3f10
Jake Soenneker <jake@soenneker.com>
2023-12-08 20:41:39 -0600

    Addt method

--- src/EnumerableStringExtension.cs
+++ src/EnumerableStringExtension.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using Soenneker.Extensions.String;
 
@@ -13,14 +14,15 @@ public static class EnumerableStringExtension
     /// <summary>
     /// Partition Key, Document Id
     /// </summary>
+    [Pure]
     public static List<Tuple<string, string>> ToSplitIds(this IEnumerable<string> ids)
     {
         var result = new List<Tuple<string, string>>();
 
         foreach (string id in ids)
         {
-            (string PartitionKey, string DocumentId) split = id.ToSplitId();
-            result.Add(new Tuple<string, string>(split.PartitionKey, split.DocumentId));
+            (string partitionKey, string documentId) = id.ToSplitId();
+            result.Add(new Tuple<string, string>(partitionKey, documentId));
         }
 
         return result;
@@ -29,8 +31,22 @@ public static class EnumerableStringExtension
     /// <summary>
     /// Checks if any item in the enumerable contains a part of a string
     /// </summary>
+    [Pure]
     public static bool ContainsAPart(this IEnumerable<string> enumerable, string part, bool ignoreCase = true)
     {
         return enumerable.Any(str => str.Contains(part, ignoreCase ? StringComparison.OrdinalIgnoreCase : StringComparison.Ordinal));
+    }
+
+    /// <summary>
+    /// Equivalent to string.Join(", ", enumerable)
+    /// </summary>
+    /// <returns>an empty string if enumerable is null</returns>
+    [Pure]
+    public static string ToCommaSeparatedString<T>(this IEnumerable<T>? enumerable)
+    {
+        if (enumerable == null)
+            return "";
+
+        return string.Join(", ", enumerable);
     }
 }
